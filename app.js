da299afd77b35c7be47f0f23cbc87cb488f17ba4
PatrikasDapsys <patrikas.dapsys@gmail.com>
2022-11-26 17:21:53 +0200

    Done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function arNidasLoyal(status) {
     if (status === "taip") {
-        document.querySelector(".answer").innerHTML = "Nemeluok! Nidas Jankauskas niekada nėra loyal.";
+        document.querySelector(".answer").innerHTML = "Nemeluok! Nidas Jankauskas nėra loyal.";
     }
     else if (status === "ne") {
         document.querySelector(".answer").innerHTML = "Teisingai! Nidas Jankauskas nėra loyal.";
